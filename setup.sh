56883478fa567eaac746ecb756afa173ad0435a4
cgalliva <cgalliva@uci.edu>
2019-03-07 15:38:56 -0800

    Disabled setup.sh since its not needed, keeping as a placeholder for now

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@
 # This will create folders in the directory above the folder where the repository was initialized by default
 
 #Folder Assignments#
-PROJECTDIR="$PWD"
-if [ ! -d "../Simulation_Data/" ]; then
-    mkdir -p ../Simulation_Data/
-fi
-
-cd ../Simulation_Data/
-RESULTSDIR="$PWD"
-
-echo PROJECTDIR=\"${PROJECTDIR}\" > ${PROJECTDIR}/.directory_save.txt
-echo RESULTSDIR=\"${RESULTSDIR}\" >> ${PROJECTDIR}/.directory_save.txt
+#PROJECTDIR="$PWD"
+#if [ ! -d "../Simulation_Data/" ]; then
+#    mkdir -p ../Simulation_Data/
+#fi
+#
+#cd ../Simulation_Data/
+#RESULTSDIR="$PWD"
+#
+#echo PROJECTDIR=\"${PROJECTDIR}\" > ${PROJECTDIR}/.directory_save.txt
+#echo RESULTSDIR=\"${RESULTSDIR}\" >> ${PROJECTDIR}/.directory_save.txt
