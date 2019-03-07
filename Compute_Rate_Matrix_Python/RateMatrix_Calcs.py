fixed probvec calculation

Compute_Rate_Matrix_Python/RateMatrix_Calcs.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy.sparse.linalg import eigs
 
 def basic_calcs(matrix, dimensions, dims_to_reduce=(2,3), nsig=15, sigma=1e-12):
+    dim = matrix.shape[0]
+    eigen_values, right_eigen_vectors = eigs(matrix, k=nsig, sigma=sigma, v0=np.ones(dim))
 
-    eigen_values, right_eigen_vectors = eigs(matrix, k=nsig, sigma=sigma)
-    prob_vec = right_eigen_vectors[:,1]/np.sum(right_eigen_vectors[:,1])
-
+    prob_vec = np.real(right_eigen_vectors[:,0])/np.real(np.sum(right_eigen_vectors[:,0]))
     prob_full_d = prob_vec.reshape(dimensions, order='F')
     prob_2d = np.sum(prob_full_d, axis=dims_to_reduce)
 