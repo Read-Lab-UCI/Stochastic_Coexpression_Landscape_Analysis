01223da95f5a1f6dbf052bfcf700e9885104487c
cgalliva <cgalliva@uci.edu>
2019-05-16 23:41:58 -0700

    Added new asym param file

--- tools/parameter_file_scripts/6D_MISAFlex_asym_params_subset.py
+++ tools/parameter_file_scripts/6D_MISAFlex_asym_params_subset.py
@@ -9,8 +9,8 @@ import scipy.io as sio
 # This script will determine the combinations of parameters
 
 # Values
-allParameters = ["N", "kd", "g0", "g1", "g2", "g3", "ha", "hr", "fa", "fr"]
-parametersToChange = [2, 3, 4, 5, 6, 7, 8, 9] # Indexes of the parameters to be varied in allParameters
+allParameters = ["N", "kd", "g0", "g1", "g2", "g3", "g0_b", "g1_b", "g2_b", "g3_b", "ha", "hr", "fa", "fr"]
+parametersToChange = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] # Indexes of the parameters to be varied in allParameters
 constants = {"N":20,
              "kd":1}  # The values for parameters not being varied
 
@@ -23,20 +23,19 @@ if __name__ == "__main__":
         csvFileName=sys.argv[1]
         print('csv file will placed in {}, be sure to change the PARAM_CSV variable in the trial_init.sh  script to refer to this name. By default trial_init looks for paramValues.csv'.format(csvFileName))
 
-    synthesis_values = np.array([0.01, 4.0])
     binding_values = np.array([1.00000000e-05, 1.00000000e-03, 1.00000000e-01, 1.00000000e+01, 1.00000000e+03])
     
     # Creating logic variation portion
-    variedLogicMatrix = sio.loadmat('gmatrix.mat')['gmatrix']
-    variedLogicMatrix = [tuple(row) for row in variedLogicMatrix]
-       
+    variedLogicMatrix = sio.loadmat('gmatrix_asym.mat')['gmatrix']
+
     # Creating logic variation portion
     variedParamMatrix = list(itertools.product(binding_values, repeat=4))
+    variedLogicMatrix = [tuple(row) for row in variedLogicMatrix]
     
     setCount = len(variedLogicMatrix) * len(variedParamMatrix)
     variedParamCount = len(parametersToChange)
     paramMatrix = np.zeros((setCount, variedParamCount))
-    
+
     i = 0
     for logic_set in variedLogicMatrix:
         for param_set in variedParamMatrix:
