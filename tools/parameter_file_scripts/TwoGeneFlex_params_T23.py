559e134eaa9a272db0f6d016584878b91b8b31d6
cgalliva <cgalliva@uci.edu>
2019-08-19 21:33:01 -0700

    4 new parameter sets

---
+++ tools/parameter_file_scripts/TwoGeneFlex_params_T23.py
@@ -0,0 +1,121 @@
+import math
+import sys
+import itertools
+import numpy as np
+import pandas as pd
+import scipy.io as sio
+
+# This script will determine the combinations of parameters
+
+# Values
+allParameters = ["N", "kd", "g0", "g1", "g2", "g3", "g0_b", "g1_b", "g2_b", "g3_b", "ha", "hr", "fa", "fr"]
+parametersToChange = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] # Indexes of the parameters to be varied in allParameters
+constants = {"N":20,
+             "kd":1}  # The values for parameters not being varied
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Missing output file path, this will be placed in this  directory.')
+        csvFileName = sys.argv[0]+'.csv'
+    else:
+        csvFileName=sys.argv[1]
+        print('csv file will placed in {}, be sure to change the PARAM_CSV variable in the trial_init.sh  script to refer to this name. By default trial_init looks for paramValues.csv'.format(csvFileName))
+
+    # Creating logic variation portion, commented out lines are 'invalid' or duplicates (double bound state directly contradicts both single bound ones)
+    productionMatrix = np.array([[1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02],
+                                 [1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00],
+                                 [1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02],
+                                 [1.e-02, 1.e-02, 4.e+00, 4.e+00, 1.e-02, 1.e-02, 4.e+00, 4.e+00],
+                                 [1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02],
+                                 [1.e-02, 4.e+00, 1.e-02, 4.e+00, 1.e-02, 4.e+00, 1.e-02, 4.e+00],
+                                 [1.e-02, 4.e+00, 4.e+00, 1.e-02, 1.e-02, 4.e+00, 4.e+00, 1.e-02],
+                                 [1.e-02, 4.e+00, 4.e+00, 4.e+00, 1.e-02, 4.e+00, 4.e+00, 4.e+00],
+                                 [4.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02],
+                                 [4.e+00, 1.e-02, 1.e-02, 4.e+00, 4.e+00, 1.e-02, 1.e-02, 4.e+00],
+                                 [4.e+00, 1.e-02, 4.e+00, 1.e-02, 4.e+00, 1.e-02, 4.e+00, 1.e-02],
+                                 [4.e+00, 1.e-02, 4.e+00, 4.e+00, 4.e+00, 1.e-02, 4.e+00, 4.e+00],
+                                 [4.e+00, 4.e+00, 1.e-02, 1.e-02, 4.e+00, 4.e+00, 1.e-02, 1.e-02],
+                                 [4.e+00, 4.e+00, 1.e-02, 4.e+00, 4.e+00, 4.e+00, 1.e-02, 4.e+00],
+                                 [4.e+00, 4.e+00, 4.e+00, 1.e-02, 4.e+00, 4.e+00, 4.e+00, 1.e-02],
+                                 [4.e+00, 4.e+00, 4.e+00, 4.e+00, 4.e+00, 4.e+00, 4.e+00, 4.e+00],
+                                # [1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02],
+                                 [1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00],
+                                 [1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02],
+                                 [1.e-02, 1.e-02, 7.e+00, 7.e+00, 1.e-02, 1.e-02, 4.e+00, 4.e+00],
+                                 [1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02],
+                                 [1.e-02, 7.e+00, 1.e-02, 7.e+00, 1.e-02, 4.e+00, 1.e-02, 4.e+00],
+                                 [1.e-02, 7.e+00, 7.e+00, 1.e-02, 1.e-02, 4.e+00, 4.e+00, 1.e-02],
+                                 [1.e-02, 7.e+00, 7.e+00, 7.e+00, 1.e-02, 4.e+00, 4.e+00, 4.e+00],
+                                 [7.e+00, 1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02],
+                                 [7.e+00, 1.e-02, 1.e-02, 7.e+00, 4.e+00, 1.e-02, 1.e-02, 4.e+00],
+                                 [7.e+00, 1.e-02, 7.e+00, 1.e-02, 4.e+00, 1.e-02, 4.e+00, 1.e-02],
+                                 [7.e+00, 1.e-02, 7.e+00, 7.e+00, 4.e+00, 1.e-02, 4.e+00, 4.e+00],
+                                 [7.e+00, 7.e+00, 1.e-02, 1.e-02, 4.e+00, 4.e+00, 1.e-02, 1.e-02],
+                                 [7.e+00, 7.e+00, 1.e-02, 7.e+00, 4.e+00, 4.e+00, 1.e-02, 4.e+00],
+                                 [7.e+00, 7.e+00, 7.e+00, 1.e-02, 4.e+00, 4.e+00, 4.e+00, 1.e-02],
+                                 [7.e+00, 7.e+00, 7.e+00, 7.e+00, 4.e+00, 4.e+00, 4.e+00, 4.e+00],
+                                # [1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02],
+                                 [1.e-02, 1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00],
+                                 [1.e-02, 1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02],
+                                 [1.e-02, 1.e-02, 4.e+00, 4.e+00, 1.e-02, 1.e-02, 7.e+00, 7.e+00],
+                                 [1.e-02, 4.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02],
+                                 [1.e-02, 4.e+00, 1.e-02, 4.e+00, 1.e-02, 7.e+00, 1.e-02, 7.e+00],
+                                 [1.e-02, 4.e+00, 4.e+00, 1.e-02, 1.e-02, 7.e+00, 7.e+00, 1.e-02],
+                                 [1.e-02, 4.e+00, 4.e+00, 4.e+00, 1.e-02, 7.e+00, 7.e+00, 7.e+00],
+                                 [4.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02],
+                                 [4.e+00, 1.e-02, 1.e-02, 4.e+00, 7.e+00, 1.e-02, 1.e-02, 7.e+00],
+                                 [4.e+00, 1.e-02, 4.e+00, 1.e-02, 7.e+00, 1.e-02, 7.e+00, 1.e-02],
+                                 [4.e+00, 1.e-02, 4.e+00, 4.e+00, 7.e+00, 1.e-02, 7.e+00, 7.e+00],
+                                 [4.e+00, 4.e+00, 1.e-02, 1.e-02, 7.e+00, 7.e+00, 1.e-02, 1.e-02],
+                                 [4.e+00, 4.e+00, 1.e-02, 4.e+00, 7.e+00, 7.e+00, 1.e-02, 7.e+00],
+                                 [4.e+00, 4.e+00, 4.e+00, 1.e-02, 7.e+00, 7.e+00, 7.e+00, 1.e-02],
+                                 [4.e+00, 4.e+00, 4.e+00, 4.e+00, 7.e+00, 7.e+00, 7.e+00, 7.e+00],
+                                # [1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02, 1.e-02],
+                                 [1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00],
+                                 [1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02],
+                                 [1.e-02, 1.e-02, 7.e+00, 7.e+00, 1.e-02, 1.e-02, 7.e+00, 7.e+00],
+                                 [1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02],
+                                 [1.e-02, 7.e+00, 1.e-02, 7.e+00, 1.e-02, 7.e+00, 1.e-02, 7.e+00],
+                                 [1.e-02, 7.e+00, 7.e+00, 1.e-02, 1.e-02, 7.e+00, 7.e+00, 1.e-02],
+                                 [1.e-02, 7.e+00, 7.e+00, 7.e+00, 1.e-02, 7.e+00, 7.e+00, 7.e+00],
+                                 [7.e+00, 1.e-02, 1.e-02, 1.e-02, 7.e+00, 1.e-02, 1.e-02, 1.e-02],
+                                 [7.e+00, 1.e-02, 1.e-02, 7.e+00, 7.e+00, 1.e-02, 1.e-02, 7.e+00],
+                                 [7.e+00, 1.e-02, 7.e+00, 1.e-02, 7.e+00, 1.e-02, 7.e+00, 1.e-02],
+                                 [7.e+00, 1.e-02, 7.e+00, 7.e+00, 7.e+00, 1.e-02, 7.e+00, 7.e+00],
+                                 [7.e+00, 7.e+00, 1.e-02, 1.e-02, 7.e+00, 7.e+00, 1.e-02, 1.e-02],
+                                 [7.e+00, 7.e+00, 1.e-02, 7.e+00, 7.e+00, 7.e+00, 1.e-02, 7.e+00],
+                                 [7.e+00, 7.e+00, 7.e+00, 1.e-02, 7.e+00, 7.e+00, 7.e+00, 1.e-02],
+                                 [7.e+00, 7.e+00, 7.e+00, 7.e+00, 7.e+00, 7.e+00, 7.e+00, 7.e+00]])
+
+    # Creating binding variation portion
+    expon_h = np.linspace(-5, 2, 5)
+    expon_f = np.linspace(-5, 5, 5)
+    print('expon_h:', expon_h)
+    print('expon_f:', expon_f)
+
+    h_vals = np.power(10, expon_h)
+    f_vals = np.power(10, expon_f)
+    print('h_vals:', h_vals)
+    print('f_vals:', f_vals)
+
+    h_vals_combinations = list(itertools.product(h_vals, repeat=2))
+    f_vals_combinations = list(itertools.product(f_vals, repeat=2))
+
+    variedBindingMatrix = list(itertools.product(h_vals_combinations, f_vals_combinations))
+    variedBindingMatrix = [i[0] + i[1] for i in variedBindingMatrix]
+
+
+    paramMatrix_seperated = list(itertools.product(productionMatrix, variedBindingMatrix))
+    paramMatrix = [list(itertools.chain.from_iterable(row)) for row in paramMatrix_seperated]
+
+    setCount = len(paramMatrix)
+    variedParamCount = len(parametersToChange)
+    
+    paramDF = pd.DataFrame(0, index=range(1,setCount+1), columns=allParameters)
+    paramDF.index.rename("paramSetNum", inplace=True)
+    
+    for key in constants.keys():
+        paramDF[key] = constants[key]
+
+    paramDF[[allParameters[i] for i in parametersToChange]] = paramMatrix
+    paramDF.to_csv(csvFileName)
